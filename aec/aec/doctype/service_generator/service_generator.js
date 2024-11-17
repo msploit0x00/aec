3eb098c1b038ba40a82c037b591e2a3c871c0e14
msploit0x00 <minasploit1@outlook.com>
2024-11-17 18:59:16 +0200

    fix is free service bug in service generator

--- aec/aec/doctype/service_generator/service_generator.js
+++ aec/aec/doctype/service_generator/service_generator.js
@@ -1,25 +1,28 @@
 frappe.ui.form.on('Service Generator', {
     validate: function(frm) {
-        let service_price_list = frm.doc.service_price_list;
+        let service_price_list = frm.doc.service_price_list || [];
         let default_count = 0;
         let default_row = null;
+
         
-        if(!service_price_list){
-            frappe.throw(__('Please add price list first'));
+        if (service_price_list.length === 0 && frm.doc.is_free_service == 0) {
+            frappe.throw(__('Please add a price list first.'));
         }
 
-        if(service_price_list.length > 0){
-        for (let row of service_price_list) {
-            if (row.is_default === 1) {
-                default_count++;
-                default_row = row;
-            }
+        if (service_price_list.length > 0 && frm.doc.is_free_service == 0) {
+            for (let row of service_price_list) {
+                if (row.is_default === 1) {
+                    default_count++;
+                    default_row = row;
+                }
 
-            if (default_count > 1) {
-                frappe.throw(__('Only one service price list can be set as default.'));
+                if (default_count > 1) {
+                    frappe.throw(__('Only one service price list can be set as default.'));
+                }
             }
+        } else {
+            console.log("Done");
         }
-    }
     }
 });
 
@@ -27,6 +30,7 @@ frappe.ui.form.on('Service Price List', {
     before_save: function(frm, cdt, cdn) {
         let row = frappe.get_doc(cdt, cdn);
         
+        // if(frm.doc.is_free_service == 0){
         if (row.is_default) {
         
             frm.doc.service_price_list.forEach(r => {
@@ -37,5 +41,7 @@ frappe.ui.form.on('Service Price List', {
 
             frm.refresh_field('service_price_list'); 
         }
+
+    // }
     }
 });
