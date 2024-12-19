b61ac6cb2fb73c2780a6a4583bc28dd3a98041a8
msploit0x00 <minasploit1@outlook.com>
2024-12-19 15:06:54 +0200

    AR customizations

---
+++ aec/aec/doctype/export_volume_categories/export_volume_categories.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, ds and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Export Volume Categories', {
+	// refresh: function(frm) {
+
+	// }
+});
