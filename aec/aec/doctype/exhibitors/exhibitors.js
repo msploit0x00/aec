3df18df98883a4dd8494c88902ac82882dfeeffd
OHany1999 <omar1999410@gmail.com>
2025-01-14 13:17:34 +0200

    sending mail task made by omar

--- aec/aec/doctype/exhibitors/exhibitors.js
+++ aec/aec/doctype/exhibitors/exhibitors.js
@@ -1,8 +1,48 @@
-// Copyright (c) 2024, ds and contributors
-// For license information, please see license.txt
+
+
+// frappe.ui.form.on('Exhibitors', {
+// 	// refresh: function(frm) {
+
+// 	// }
+// });
+
 
 frappe.ui.form.on('Exhibitors', {
-	// refresh: function(frm) {
+    send_email: function(frm) {
+        const sender_mail = frm.doc.sender_email;
 
-	// }
+		// var sender = frappe.model.get_value("Email Account", sender_mail,'email_id');
+
+		var sender = frm.doc.email;
+
+
+        const subject = frm.doc.subject;
+        const body_mail = frm.doc.body_mail;
+        const list_reciver = frm.doc.email_ids.map(function(row){
+			return row.email_id;
+		});
+		console.log(list_reciver);
+
+        if (!sender_mail || !subject || !body_mail || list_reciver.length === 0) {
+            frappe.msgprint(__("Please fill all the required fields (Sender Mail, Subject, Body Mail, Table of emails is empty )."));
+            return;
+        }
+
+        frappe.call({
+            method: 'aec.aec.doctype.exhibitors.exhibitors.send_email_list',
+            args: {
+                sender_mail: sender,
+                subject: subject,
+                body_mail: body_mail,
+				list_reciver: list_reciver,
+            },
+            callback: function(r) {
+                if (r.message) {
+                    frappe.msgprint(__("Email sent successfully!"));
+                } else {
+                    frappe.msgprint(__("Failed to send email. Please try again."));
+                }
+            }
+        });
+    }
 });
--- aec/aec/doctype/exhibitors/exhibitors.py
+++ aec/aec/doctype/exhibitors/exhibitors.py
@@ -1,8 +1,25 @@
 # Copyright (c) 2024, ds and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
+
 
 class Exhibitors(Document):
 	pass
+
+
+@frappe.whitelist()
+def send_email_list(sender_mail, subject, body_mail,list_reciver):
+    try:
+        # frappe.msgprint('list of reciver is: '+ list_reciver)
+        frappe.sendmail(
+            sender= sender_mail,
+            recipients= list_reciver,
+            subject= subject,
+            message= body_mail
+        )
+        return "Email sent successfully!"
+    except Exception as e:
+        frappe.log_error(frappe.get_traceback(), "Failed to send email")
+        return f"Failed to send email: {str(e)}"
