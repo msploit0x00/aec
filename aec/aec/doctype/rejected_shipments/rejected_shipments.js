0583b9826caf72e993280f95a829159bed83c5c8
msploit0x00 <minasploit1@outlook.com>
2025-02-03 17:42:09 +0200

    saving all customizations from AR

---
+++ aec/aec/doctype/rejected_shipments/rejected_shipments.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, ds and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Rejected Shipments', {
+	// refresh: function(frm) {
+
+	// }
+});
---
+++ aec/aec/doctype/sub_cluster/sub_cluster.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, ds and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Sub Cluster', {
+	// refresh: function(frm) {
+
+	// }
+});
